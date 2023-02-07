088aca742430aeaa3e18f90760eeb8b3e864e643
denczik228 <bernikovden@gmail.com>
2023-02-07 03:37:04 +0200

    register rout

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+const authRouter = require('./routes/authRoute.js')
+
 //connection to db
 const mongoose = require('mongoose');
 () => {
@@ -21,6 +23,10 @@ mongoose
 // app.use("/", (req, res) => {
 //     res.send("hello 1st")
 // })
+
+//routes
+
+app.use('/api/user', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server starts on ${PORT}`)
