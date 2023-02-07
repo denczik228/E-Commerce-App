a7ec8b1f5ce5bf13bee56483e5226431d48c30f2
denczik228 <bernikovden@gmail.com>
2023-02-07 05:19:41 +0200

    set a middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const authRouter = require("./routes/authRoute.js");
+const { notFound, errorHandler } = require("./middlewares/errorHandler.js");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //routes
 app.use('/api/user', authRouter);
+
+//----------from middleware(after routes!)
+app.use(notFound);
+app.use(errorHandler);
 
 //connection to db
 const mongoose = require('mongoose');
