0664f053252e9169b249f481e40d2c9d21580011
denczik228 <bernikovden@gmail.com>
2023-02-07 04:40:06 +0200

    user controller for register rout

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 const express = require('express');
-const app = express();
-
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute.js");
 
-const authRouter = require('./routes/authRoute.js')
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//routes
+app.use('/api/user', authRouter);
 
 //connection to db
 const mongoose = require('mongoose');
-() => {
-try{
+
 mongoose
         .set("strictQuery", false)
         .connect(process.env.MONGO_URL)
         console.log("DB was connected")
-    } catch (err) {
-        console.log(err)
-    }
-}
-
-//for 1st testing
-// app.use("/", (req, res) => {
-//     res.send("hello 1st")
-// })
-
-//routes
-
-app.use('/api/user', authRouter);
-
+ 
 app.listen(PORT, () => {
     console.log(`Server starts on ${PORT}`)
 });
