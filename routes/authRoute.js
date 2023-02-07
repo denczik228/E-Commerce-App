a013c4f0225a80f652785d24b0ab70828d02a45e
denczik228 <bernikovden@gmail.com>
2023-02-08 01:50:37 +0200

    functions-get all users/get user/delete user/update user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const { createUser, loginUser } = require('../controllers/userController');
+const {
+  createUser,
+  loginUser,
+  getAllUser,
+  getUser,
+  updateUser,
+  delUser,
+} = require("../controllers/userController");
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
+router.get('/all', getAllUser);
+router.get('/:id', getUser);
+router.delete('/:id', delUser);
+router.put('/:id', updateUser);
 
 module.exports = router;
