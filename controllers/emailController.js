cd9fd98b4151b9dd98904c2ccd21eae69aeaf739
denczik228 <bernikovden@gmail.com>
2023-02-13 03:37:26 +0200

    nodemailer - edited usermodel for reseting password

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -28,10 +28,6 @@ const sendEmail = asyncHandler(async (data, req, res) => {
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-    main().catch(console.error);
-
 })
 
 module.exports = sendEmail;
